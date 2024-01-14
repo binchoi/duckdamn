6dafecb9e3b6429e94fdd19e9047621f7a8812c4
Bin Choi <binchoi@u.yale-nus.edu.sg>
2024-01-14 17:43:05 +0900

    Remove unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
         }}
         onSubmit={(
           data,
-          { completeForm, setIsSubmitting, goToBlock, setSubmissionErr },
+          { completeForm, setIsSubmitting },
         ) => {
           setTimeout(() => {
             setIsSubmitting(false);
